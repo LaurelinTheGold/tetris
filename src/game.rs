9685d5093df01abc1eb6699d9ce31a50db9c520f
Richard Chen <richard.rl.chen@gmail.com>
2021-03-19 01:22:54 -0400

    display should work, using crossterm

--- src/game.rs
+++ src/game.rs
@@ -1,4 +1,5 @@
 pub(crate) const BOARD_HEIGHT: usize = 24;
+pub(crate) const BOARD_HEIGHT_SHOWN: usize = 20;
 pub(crate) const BOARD_WIDTH: usize = 10;
 const FULL_ROW: u16 = 1023; //2 ^ 10 - 1
 
@@ -21,9 +22,6 @@ impl GameState {
     fn new(board: [u16; BOARD_HEIGHT], score: u16) -> Self {
         Self { board, score }
     }
-    pub(crate) fn make_test(board: [u16; BOARD_HEIGHT]) -> Self {
-        Self { board, score: 0 }
-    }
     fn clear_rows(&mut self) -> () {
         let mut count: u16 = 0;
         for row in self.board.iter_mut() {
@@ -37,39 +35,25 @@ impl GameState {
     pub(crate) fn make_game() -> Self {
         GameState::new([0; BOARD_HEIGHT], 0)
     }
-    pub(crate) fn do_thing(&mut self) -> () {
+    pub(crate) fn advance_timestep(&mut self) -> () {
         self.score += 1;
         self.clear_rows();
-        // println!("Tetris! {}", self.score);
     }
     pub(crate) fn get_board(&self) -> [u16; BOARD_HEIGHT] {
         self.board
+    }
+
+    pub(crate) fn rotate_piece(&mut self) -> () {
+        //
+    }
+    pub(crate) fn move_left(&mut self, left: bool) -> () {
+        //
+    }
+    pub(crate) fn move_down(&mut self) -> () {
+        //
     }
 }
 
 fn row_full(row: u16) -> bool {
     row > FULL_ROW
 }
-
-// struct Example {
-//     number: i32,
-// }
-// impl Example {
-//     fn boo() {
-//         println!("boo! Example::boo() was called!");
-//     }
-//     fn answer(&mut self) {
-//         self.number += 42;
-//     }
-//     fn get_number(&self) -> i32 {
-//         self.number
-//     }
-// }
-// trait Thingy {
-//     fn do_thingy(&self);
-// }
-// impl Thingy for Example {
-//     fn do_thingy(&self) {
-//         println!("doing a thing! also, number is {}!", self.number);
-//     }
-// }
--- src/main.rs
+++ src/main.rs
@@ -2,79 +2,120 @@
 #[macro_use]
 extern crate crossterm;
 
-use crossterm::cursor;
-use crossterm::event::{read, Event, KeyCode, KeyEvent, KeyModifiers};
-use crossterm::style::Print;
-use crossterm::terminal::{disable_raw_mode, enable_raw_mode, Clear, ClearType};
-use std::io::{stdout, Write};
+use core::time::Duration;
+use crossterm::event::poll;
+use crossterm::terminal::size;
+use crossterm::terminal::{disable_raw_mode, enable_raw_mode, ScrollUp};
+use crossterm::{
+    event::{read, Event, KeyCode, KeyEvent, KeyModifiers},
+    terminal::SetSize,
+};
+use display::draw_game;
+use game::GameState;
+use std::{
+    io::stdout,
+    sync::mpsc::{channel, Receiver, Sender},
+    thread::{self},
+    u16,
+};
 
+use std::io::stderr;
 use std::thread::sleep;
 
 mod display;
 mod game;
 
-fn main() {
-    println!("{}", display::TWENTY_NEWLINES);
+const ONE_SEC: Duration = Duration::from_secs(1);
+const FPS: f32 = 60.; // number of polls per sec
+const TIME_STEP: f32 = 1.; // in seconds
 
-    let mut stdout = stdout();
-    //going into raw mode
-    enable_raw_mode().unwrap();
-
-    //clearing the screen, going to top left corner and printing welcoming message
-    execute!(stdout, 
-        Clear(ClearType::All), 
-        cursor::MoveTo(0, 0), 
-        Print(r#"ctrl + q exit, ctrl + h "Hello world", alt + t "crossterm is cool""#))
-            .unwrap();
-
-
-    let mut game_tick: usize = 0;
+fn main() -> () {
+    let (cols, rows, tx, rx): (u16, u16, Sender<bool>, Receiver<bool>) = setup();
     let mut tetris_game = game::GameState::make_game();
     loop {
-        //going to top left corner
-        execute!(stdout, cursor::MoveTo(0, 0)).unwrap();
-
-        //matching the key
-        match read().unwrap() {
-            //i think this speaks for itself
-            Event::Key(KeyEvent {
-                code: KeyCode::Char('h'),
-                modifiers: KeyModifiers::CONTROL,
-                //clearing the screen and printing our message
-            }) => execute!(stdout, Clear(ClearType::All), Print("Hello world!")).unwrap(),
-            Event::Key(KeyEvent {
-                code: KeyCode::Char('t'),
-                modifiers: KeyModifiers::ALT,
-            }) => execute!(stdout, Clear(ClearType::All), Print("crossterm is cool")).unwrap(),
-            Event::Key(KeyEvent {
-                code: KeyCode::Char('q'),
-                modifiers: KeyModifiers::CONTROL,
-            }) => break,
-            _ => (),
+        let thread_tx = tx.clone();
+        // Thread that waits for a time then signals before ending
+        let timer_thread = thread::spawn(move || {
+            sleep(ONE_SEC.mul_f32(TIME_STEP)); // wait for time
+            let _ = thread_tx.send(true); // use channel to signal about to done
+        });
+        while rx.try_recv().is_err() {
+            if poll_for_keystroke(&mut tetris_game) {
+                cleanup(cols, rows);
+                return;
+            }
+            sleep(ONE_SEC.div_f32(FPS)); // wait
         }
-
-        match read().unwrap() {
-            Event::Key(KeyEvent {
-                code: KeyCode::Char('a'),
-                modifiers: no_modifiers,
-            }) => //--code--
-            Event::Mouse(_) => {}
-            Event::Resize(_, _) => {}
-        }
-
-        // display::draw_game(&tetris_game);
-        display::draw_test(game_tick);
-        tetris_game.do_thing();
-        game_tick += 1;
-        // println!("Tick {}", game_tick);
-        sleep(core::time::Duration::from_secs(1));
-        display::clear_screen();
+        let _ = timer_thread.join().unwrap();
+        GameState::advance_timestep(&mut tetris_game);
+        draw_game(&tetris_game, &mut stdout());
     }
 }
 
-/*
-board width 10
-board height 20 on screen 24 total
-tetrominos I T Z S J L O rotated
+fn poll_for_keystroke(game: &mut GameState) -> bool {
+    if poll(Duration::from_secs(0)).unwrap() {
+        // true means found
+        let event: crossterm::event::Event = read().unwrap();
+        match event {
+            Event::Key(KeyEvent {
+                code: KeyCode::Char('q'),
+                modifiers: KeyModifiers::NONE,
+            }) => {
+                return true;
+            }
+            Event::Key(KeyEvent {
+                code: KeyCode::Char('w'),
+                modifiers: KeyModifiers::NONE,
+            }) => {
+                game.rotate_piece();
+                draw_game(game, &mut stderr());
+            }
+            Event::Key(KeyEvent {
+                code: KeyCode::Char('a'),
+                modifiers: KeyModifiers::NONE,
+            }) => {
+                game.move_left(true);
+                draw_game(game, &mut stderr())
+            }
+            Event::Key(KeyEvent {
+                code: KeyCode::Char('d'),
+                modifiers: KeyModifiers::NONE,
+            }) => {
+                game.move_left(false);
+                draw_game(game, &mut stderr())
+            }
+            Event::Key(KeyEvent {
+                code: KeyCode::Char('s'),
+                modifiers: KeyModifiers::NONE,
+            }) => {
+                game.move_down();
+                draw_game(game, &mut stderr())
+            }
+            _ => {}
+        };
+    }
+    return false;
+}
 
-*/
+fn setup() -> (u16, u16, Sender<bool>, Receiver<bool>) {
+    let (cols, rows) = size().unwrap();
+    println!("Press Enter to play tetris. Q quits. ");
+    enable_raw_mode().unwrap();
+    while read().unwrap() != Event::Key(KeyCode::Enter.into()) {}
+    execute!(
+        stdout(),
+        SetSize(
+            game::BOARD_WIDTH as u16 * 2,
+            game::BOARD_HEIGHT_SHOWN as u16
+        ),
+        ScrollUp(5) // TODO what is this???
+    )
+    .unwrap();
+    let (t, r) = channel();
+    return (cols, rows, t, r);
+}
+
+fn cleanup(cols: u16, rows: u16) -> () {
+    execute!(stdout(), SetSize(cols, rows)).unwrap();
+    disable_raw_mode().unwrap();
+}
